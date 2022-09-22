ba12468a16836ac54ccd1ba3bf479e0fa03b24b3
polyathlon <polyatlon.rsu.edu.ru@gmail.com>
2022-09-22 16:53:08 +0300

    Change Logo

--- js/list.js
+++ js/list.js
@@ -1,6 +1,6 @@
 var List = [
 	{ type: 'naive vue', id: 'vue', url: './frameworks/vue', label: 'Vue' },
-	{ type: 'naive odajs', id: 'odajs', url: './frameworks/odajs', label: 'ODAJS', hide_label: true },
+	{ type: 'naive odajs', id: 'odajs', url: './frameworks/odajs', label: 'ODAJS', hide_label: false },
 	{ type: 'naive lit', id: 'lit', url: './frameworks/lit', label: 'Lit'},
 	{ type: 'naive aurelia', id: 'aurelia', url: './frameworks/aurelia', label: 'Aurelia' },
 	{ type: 'naive backbone', id: 'backbone', url: './frameworks/backbone', label: 'Backbone' },
@@ -9,4 +9,5 @@ var List = [
 	{ type: 'naive react', id: 'react', url: './frameworks/react', label: 'React' },
 	{ type: 'optimized preact', id: 'preact', url: './frameworks/preact', label: 'Preact' },
 	{ type: 'naive mithril', id: 'mithril', url: './frameworks/mithril', label: 'Mithril' },
+	{ type: 'naive svelte', id: 'svelte', url: './frameworks/svelte', label: 'Svelte' },
 ];
